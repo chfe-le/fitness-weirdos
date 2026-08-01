16558d8ec3333e33f40fb6cc08b78adaf3c54e11
chfe-le <fp3969@wayne.edu>
2026-08-01 19:23:29 -0400

    Fix formatting issue in script.js
    missed a bracket

--- script.js
+++ script.js
@@ -224,4 +224,4 @@ if (numChildrenSelect) {
                         childrenContainer.appendChild(fieldset);
                         }
                         });
-                            
+                        }    
